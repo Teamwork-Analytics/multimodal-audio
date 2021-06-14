6f736e275c981e12f362428cf43b293ceb175a17
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2021-06-14 23:10:29 +1000

    Add several JavaFX files (from tutorial)

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,36 +1,53 @@
 package main;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Pos;
+
 import javafx.scene.Scene; // content inside the window
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage; // entire window
 import jfxtras.styles.jmetro.JMetro;
+import jfxtras.styles.jmetro.JMetroStyleClass;
 import jfxtras.styles.jmetro.Style;
 
-public class Main extends Application {
-    Button button;
+public class Main extends Application{
+    Stage window;
+    Scene mainScene, secondScene;
     JMetro jMetro = new JMetro(Style.LIGHT);
 
     public static void main(String[] args) {
         launch(args); // set up for JavaFX application
-//        jMetro.setScene(scene);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("Multimodal Audio Mixer - MTA");
-        button = new Button();
-        button.setText("Click me");
+        window = stage;
+        Label label1 = new Label("Select the microphone");
+        Button button1 = new Button("Go to next scene");
+        button1.setOnAction(e -> {
+            boolean res = ConfirmBox.display("Title of Window", "Yay!");
+            System.out.println(res);
+        });
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
+        window.setOnCloseRequest(e -> window.close());
 
-        Scene scene = new Scene(layout, 300, 250);
-        jMetro.setScene(scene);
-        stage.setScene(scene);
-        stage.show();
+        // Layout 1 - vertical column
+        VBox layout1 = new VBox(100); // stack layout
+        layout1.getStyleClass().add(JMetroStyleClass.BACKGROUND);
+        layout1.getChildren().addAll(label1, button1);
+        mainScene = new Scene(layout1, 200, 200);
+
+
+        jMetro.setScene(mainScene);
+        window.setScene(mainScene);
+        window.setTitle("Multimodal Audio Mixer - MTA");
+        window.show();
     }
+
+    private void close(){
+
+    }
+
+
+
 }
