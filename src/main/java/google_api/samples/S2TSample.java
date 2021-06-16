85fce70af0a077a209be6bd057a600f834ac70a1
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2021-06-17 08:47:31 +1000

    Update speech to text sample that uses microphone (simplest one).

--- src/main/java/google_api/samples/S2TSample.java
+++ src/main/java/google_api/samples/S2TSample.java
@@ -7,9 +7,20 @@ import com.google.cloud.speech.v1.*;
 import com.google.protobuf.ByteString;
 
 import javax.sound.sampled.*;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class S2TSample {
+
+    public static void main(String[] args) {
+        try {
+            streamingMicRecognize();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
     /** Performs microphone streaming speech recognition with a duration of 1 minute. */
     public static void streamingMicRecognize() throws Exception {
 
@@ -24,6 +35,7 @@ public class S2TSample {
 
                         public void onResponse(StreamingRecognizeResponse response) {
                             responses.add(response);
+                            System.out.println(response);
                         }
 
                         public void onComplete() {
@@ -45,11 +57,11 @@ public class S2TSample {
             RecognitionConfig recognitionConfig =
                     RecognitionConfig.newBuilder()
                             .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
-                            .setLanguageCode("en-AU")
+                            .setLanguageCode("en-US")
                             .setSampleRateHertz(16000)
                             .build();
             StreamingRecognitionConfig streamingRecognitionConfig =
-                    StreamingRecognitionConfig.newBuilder().setConfig(recognitionConfig).build();
+                    StreamingRecognitionConfig.newBuilder().setConfig(recognitionConfig).setSingleUtterance(true).build();
 
             StreamingRecognizeRequest request =
                     StreamingRecognizeRequest.newBuilder()
@@ -62,19 +74,39 @@ public class S2TSample {
             // SampleRate:16000Hz, SampleSizeInBits: 16, Number of channels: 1, Signed: true,
             // bigEndian: false
             AudioFormat audioFormat = new AudioFormat(16000, 16, 1, true, false);
+            Mixer.Info[] mixerInfos = AudioSystem.getMixerInfo(); // get all mixers data
+            HashMap<Integer, Mixer> mixerHashMap = new HashMap<>();
+            int i = 0;
+            for(Mixer.Info info : mixerInfos){
+                Mixer mixer = AudioSystem.getMixer(info);
+                Line.Info[] lineInfos = mixer.getTargetLineInfo();
+                if(lineInfos.length>=1 && lineInfos[0].getLineClass().equals(TargetDataLine.class)){//Only prints out info is it is a Microphone
+                    System.out.printf("%d. %s\n", i, info.getName());
+                    mixerHashMap.put(i, mixer);
+                    i++;
+                }
+            }
+
+            System.out.println("Select audio input:");
+            Scanner scanner = new Scanner(System.in);
+            int index = scanner.nextInt();
+            Mixer mixer = mixerHashMap.get(index);
 
             // Set the system information to read from the microphone audio stream
             DataLine.Info targetInfo = new DataLine.Info( TargetDataLine.class, audioFormat);
 
-            if (!AudioSystem.isLineSupported(targetInfo)) {
+            if (!mixer.isLineSupported(targetInfo)) {
                 System.out.println("Microphone not supported");
                 System.exit(0);
             }
             // Target data line captures the audio stream the microphone produces.
-            TargetDataLine targetDataLine = (TargetDataLine) AudioSystem.getLine(targetInfo);
+            TargetDataLine targetDataLine = (TargetDataLine) mixer.getLine(targetInfo);
+
             targetDataLine.open(audioFormat);
             targetDataLine.start();
             System.out.println("Start speaking");
+
+            File audioFile = new File("test.wav");
 
             // TODO: Audio Input Stream, it starts here
             long startTime = System.currentTimeMillis();
@@ -83,18 +115,23 @@ public class S2TSample {
                 long estimatedTime = System.currentTimeMillis() - startTime;
                 byte[] data = new byte[6400];
                 audio.read(data);
-                if (estimatedTime > 60000) { //TODO: 60 seconds speaking time
+                if (estimatedTime > 15000) { //TODO: 60 seconds speaking time
                     System.out.println("Stop speaking.");
                     targetDataLine.stop();
                     targetDataLine.close();
                     break;
                 }
+
                 request =
                         StreamingRecognizeRequest.newBuilder()
                                 .setAudioContent(ByteString.copyFrom(data))
                                 .build();
                 clientStream.send(request);
             }
+            AudioSystem.write(audio, AudioFileFormat.Type.WAVE, audioFile);
+
+            System.out.println("Stopped Recording");
+
         } catch (Exception e) {
             e.printStackTrace();
         }
