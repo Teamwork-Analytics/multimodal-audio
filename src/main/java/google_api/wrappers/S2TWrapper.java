2ec1eb97d5a6b04dcc1ad38b87ab22a9669cca5e
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2021-06-17 14:52:22 +1000

    create a new wrapper

---
+++ src/main/java/google_api/wrappers/S2TWrapper.java
@@ -0,0 +1,41 @@
+package google_api.wrappers;
+
+import com.google.cloud.speech.v1.SpeechClient;
+import google_api.SpeechToTextAPI;
+import microphone.MultimodalMixer;
+
+import java.io.IOException;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class S2TWrapper {
+    /**
+     * A shared queue
+     * Must be static and volatile because it must retain its existence in memory
+     */
+    private static volatile BlockingQueue<byte[]> sharedQueue = new LinkedBlockingQueue<>();
+    private SpeechToTextAPI s2t;
+    private MultimodalMixer multimodalMixer;
+
+    public S2TWrapper(MultimodalMixer multimodalMixer){
+        this.s2t = new SpeechToTextAPI();
+        this.multimodalMixer = new MultimodalMixer(s2t);
+    }
+
+    public void useSpeechToText(String mixerName){
+        try (SpeechClient speechClient = SpeechClient.create()) {
+            this.s2t.initConfig(speechClient);
+            this.multimodalMixer.listen(mixerName);
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void stop(){
+        this.multimodalMixer.stop();
+        s2t.transcribe();
+    }
+
+
+}
